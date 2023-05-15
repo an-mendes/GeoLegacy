ea9d191300fede2773623f36dbe74b90aa1fab29
Antonio Mendeš <antoni066.psn@gmail.com>
2023-05-15 16:50:01 +0200

    Added more comments

--- app/src/main/java/com/example/geolegacy/MainActivity.kt
+++ app/src/main/java/com/example/geolegacy/MainActivity.kt
@@ -13,7 +13,6 @@ import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.tasks.Task
 
-
 class MainActivity : AppCompatActivity() {
     private val REQUEST_CHECK_SETTINGS = 123
     lateinit var binding: ActivityMainBinding
@@ -23,12 +22,16 @@ class MainActivity : AppCompatActivity() {
     private var mCurrentLocation: Location? = null
     private lateinit var mLocationRequestBalancedPowerAccuracy: LocationRequest
     private lateinit var mLocationRequestHighAccuracy: LocationRequest
+
+    // Called when the activity is resumed
     override fun onResume() {
         super.onResume()
         if (binding.swLocationsupdates.isChecked) startLocationUpdates()
     }
 
+    // Starts location updates
     private fun startLocationUpdates() {
+        // Check if location permissions are granted
         if (ActivityCompat.checkSelfPermission(
                 this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION
@@ -38,14 +41,16 @@ class MainActivity : AppCompatActivity() {
             ) !=
             PackageManager.PERMISSION_GRANTED
         ) {
-            // only requests fine permissions, may need to add coarse locations 1st cause of
-            //potential bugs with android 11
+            // Request location permissions if not granted
+            // Only requests fine permissions, may need to add coarse locations 1st cause of
+            // potential bugs with Android 11
             ActivityCompat.requestPermissions(
-                this, arrayOf
-                    (android.Manifest.permission.ACCESS_FINE_LOCATION), 101
+                this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101
             )
             return
         }
+
+        // Request location updates with high accuracy
         fusedLocationClient.requestLocationUpdates(
             mLocationRequestHighAccuracy,
             locationCallback,
@@ -53,56 +58,66 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    // creating an instance of fusedLocationClient before onCreate
+    // Creating an instance of fusedLocationClient before onCreate
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //initializing binding variable
+        // Initializing binding variable
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.swLocationsupdates.isChecked = false
+
+        // Creating location request for high accuracy
         mLocationRequestHighAccuracy = LocationRequest.create().apply {
             interval = 10000
             fastestInterval = 5000
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
+
+        // Creating location request for balanced power accuracy
         mLocationRequestBalancedPowerAccuracy = LocationRequest.create().apply {
             interval = 35000
             fastestInterval = 30000
             priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
         }
+
+        // Building location settings request with high accuracy and balanced power accuracy requests
         val builder = LocationSettingsRequest.Builder()
             .addLocationRequest(mLocationRequestHighAccuracy)
             .addLocationRequest(mLocationRequestBalancedPowerAccuracy)
-        // ??
+
+        // Getting the settings client
         val client: SettingsClient = LocationServices.getSettingsClient(this)
-        // ??
+
+        // Checking location settings
         val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
             .addOnSuccessListener { LocationSettingsResponse ->
-                //good i guess?
+                // Location settings are satisfied
             }
-        // this entire thing is hella sus
+
         task.addOnFailureListener { exception ->
             if (exception is ResolvableApiException) {
-                //TODO implement this
-                // location settings are not satisfied, but is fixable
-                //TODO by giving the user a dialogue box
+                // Location settings are not satisfied but fixable
+                // Show a dialogue box to the user to enable location settings
                 try {
                     exception.startResolutionForResult(this@MainActivity, REQUEST_CHECK_SETTINGS)
                 } catch (sendEx: IntentSender.SendIntentException) {
-                    //ignore this error
+                    // Ignore this error
                 }
             }
         }
 
-
-        //TODO request location updates
-
+        // Getting the FusedLocationProviderClient instance
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Getting the current location
         getCurrentlocation()
-        //TODO fix this
+
+        // Setting up the location callback
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult) {
+                // Handling the location result
                 for (location in locationResult.locations) {
                     latitude = location.latitude
                     longitude = location.longitude
@@ -111,6 +126,8 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        // Check if location permissions are granted
         if (ActivityCompat.checkSelfPermission(
                 this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION
@@ -120,30 +137,38 @@ class MainActivity : AppCompatActivity() {
             ) !=
             PackageManager.PERMISSION_GRANTED
         ) {
+            // Request location updates if permissions are granted
             fusedLocationClient.requestLocationUpdates(
                 mLocationRequestHighAccuracy,
                 Looper.getMainLooper()
-
             )
         }
+
+        // Set an onCheckedChangeListener for the switch button
         binding.swLocationsupdates.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
+                // Start location updates when the switch button is checked
                 startLocationUpdates()
             } else {
+                // Stop location updates when the switch button is unchecked
                 stopLocationUpdates()
                 binding.tvLon.text = 0.toString()
                 binding.tvLat.text = 0.toString()
             }
         }
+
         onResume()
     }
-    //TODO ^^ THIS IS ON CREATE END
+    // TODO ^^ THIS IS ON CREATE END
+
+    // Stops location updates
     private fun stopLocationUpdates() {
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
-
+    // Retrieves the current location
     private fun getCurrentlocation() {
+        // Checks if location permissions are granted
         fun checkPermissions() {
             if (ActivityCompat.checkSelfPermission(
                     this,
@@ -154,24 +179,24 @@ class MainActivity : AppCompatActivity() {
                 ) !=
                 PackageManager.PERMISSION_GRANTED
             ) {
-                // only requests fine permissions, may need to add coarse locations 1st cause of
-                //potential bugs with android 11
+                // Only requests fine permissions, may need to add coarse locations 1st cause of
+                // potential bugs with Android 11
                 ActivityCompat.requestPermissions(
-                    this, arrayOf
-                        (android.Manifest.permission.ACCESS_FINE_LOCATION), 101
+                    this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101
                 )
                 return
             }
         }
+
         checkPermissions()
-        //^^ checking permissions DON'T TOUCH
+
+        // Retrieves the last known location
         fun lastLocation() {
             fusedLocationClient.lastLocation
                 .addOnSuccessListener { location: Location? ->
-                    // since Location is a nullable object, location?.let handles cases
-                    //in which location can be null, in which case the code does not get executed
+                    // Handles the case when location is not null
                     location?.let {
-                        // storing location in mCurrentLocation
+                        // Storing location in mCurrentLocation
                         mCurrentLocation = it
                         latitude = location.latitude
                         longitude = location.longitude
@@ -183,14 +208,16 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "Failed to get location", Toast.LENGTH_SHORT).show()
                 }
         }
+
         lastLocation()
     }
-
 }
 
+// Function required for looper to work
 private fun FusedLocationProviderClient.requestLocationUpdates(
     mLocationRequestHighAccuracy: LocationRequest,
     mainLooper: Looper?
 ) {
-
+    // Function to request location updates (implementation is missing)
 }
+
